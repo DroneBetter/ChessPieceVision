3efe53a584275fa4dfac57d2a436269a67855428
Drone_Better <58664547+DroneBetter@users.noreply.github.com>
2022-02-18 22:02:15 +0000

    (Adds itertools as dependency) Make state transition graphs work with more pieces than only two kings, finish knight movegen, optimise physics function (and fix slice notation bug where indices were off by one (causing graph embeddings to be extremely suboptimal in previous 3 commits))
    itertools is now a dependency because I didn't want to reimplement its combinations and permutations functions myself (though I may in the future), all permutations of each combination of non-king pieces and empty squares fill the gaps that each of the invariant legal king positions create.
    
    (Optimisation done because exponentiating by 1.5 is faster than by 3 then square-rooting with the math function, and di didn't need to be an indice in air resistance and velocity enaction loops.)
    Running it now creates a state transition diagram for the 512 possible positions of a king and knight vs. king on 3*3 (in which checkmate is impossible but it converges to the shape of a porcupine or pineapple due to stalemated positions (where the black king is in a corner and the white is on an edge with its knight on a corner adjacent to it) not having any moves acting as springs to pull them back in), I think if you run it for long enough you'll see a 64-node graph escape from the main one (which is the same as the one without a knight, but is where the knight is on the centre square and can't move (like the meme format) and also can't be taken). If you're suspicious of the number being a power of two, there are 64 positions with no knight, 64*6 where the knight is useful and 64 where it's useful, 64*8 in total.
    
    Next, I will add zooming (perhaps not automatic but using screen size better than now (perhaps fitting the minimal circumscribing sphere to the square screen area for determining the centre instead of taking the mean) and zooming based on maximum spatial difference (not screen difference because then it would zoom in and out as you rotate)), iterative pieces (ie. queens/rooks/bishops/nightriders that require the board state to be inputted to them also) and checkmate regression (so it will prune the graph), and thus colour-coding of graph nodes based on who's winning (with a small difference in shade based on whose turn it is) and the ability to play against you (where you input PGN moves) in the command line.

--- tablebase vision.py
+++ tablebase vision.py
@@ -1,4 +1,5 @@
 import math, pygame, random
+from sympy.utilities.iterables import multiset_permutations, combinations #generates permutations where elements are identical, from https://stackoverflow.com/a/60252630
 from pygame.locals import *
 clock=pygame.time.Clock()
 
@@ -7,6 +8,26 @@ boardSquares=boardWidth**2
 board=[]*boardSquares
 pieceNames=["empty","king","queen","rook","bishop","knight","pawn"]
 pieceSymbols=[[" ","k","q","r","b","n","p"],[" ","K","Q","R","B","N","P"]]
+def parseMaterial(materialString): #I think in the endgame material notation, the piece letters are all capitalised but the v isn't
+    vLoc=materialString.index("v")
+    return [[pieceSymbols[1].index(i),0] for i in materialString[:vLoc] if i!="K"]+[[pieceSymbols[1].index(i),1] for i in materialString[vLoc+1:] if i!="K"] #not sure how to shorten (elses don't seem to work in append-forgoing if statements in list comprehensions)
+def generateCombinations(material):
+    duplicateCombinations=[list(c) for m in range(len(material)+1) for c in combinations(material,m)]
+    nonDuplicateCombinations=[]
+    for c in duplicateCombinations:
+        if c not in nonDuplicateCombinations:
+            nonDuplicateCombinations.append(c)
+    return nonDuplicateCombinations
+combinations=generateCombinations(parseMaterial("KNvK"))
+print("combinations:",combinations)
+
+def generatePermutations(material,squares):
+    thingsToPermute=material+[[0,0]]*squares
+    return list(multiset_permutations(thingsToPermute))
+
+def appendMove(relativeMovement):
+    return position+relativeMovement[0]+boardWidth*relativeMovement[1]
+
 precomputedKnightMoves=[]
 def findPieceMoves(piece,position): #outputs list of squares to which piece can travel
     if piece==0:
@@ -32,58 +53,74 @@ def findPieceMoves(piece,position): #outputs list of squares to which piece can 
         if position<boardSquares-boardWidth: #not on top row
             moves+=[i+boardWidth for i in horizontalMoves]
     elif piece==5: #extremely inelegant but I will call it hardcoded instead
-        #return precomputedKnightMoves[position]
+        if knightMovesPrecomputed:
+            return precomputedKnightMoves[position]
         #xMovement=max(xPosition-boardWidth+3,0) if xPosition>1 else -1-(xPosition==0) #removed because I realised it doesn't work with 3*3 boards (where it's both 1 and -1)
-        sideObstructions=[max(2-xPosition,0),max(xPosition-boardWidth+3,0),(position<2*boardWidth)*(1+(position<boardWidth)),(position>=boardSquares-2*boardWidth)*(1+(position>=boardSquares-boardWidth))] #directions [left,right,down,up], 0 is unobstructed, 1 is next to edge, 2 is on edge
+        sideObstructions=[max(2-xPosition,0),max(xPosition-boardWidth+3,0),(position<2*boardWidth)+(position<boardWidth),(position>=boardSquares-2*boardWidth)+(position>=boardSquares-boardWidth)] #directions [left,right,down,up], 0 is unobstructed, 1 is next to edge, 2 is on edge
         if sideObstructions[0]<2:
+            if sideObstructions[2]==0:
+                moves.append((-1,-2))
             if sideObstructions[3]==0:
-                moves+=(-1,-2)
-            if sideObstructions[4]==0:
-                moves+=(-1,2)
+                moves.append((-1,2))
             if sideObstructions[0]==0:
+                if sideObstructions[2]<2:
+                    moves.append((-2,-1))
                 if sideObstructions[3]<2:
-                    moves+=(-2,-1)
-                if sideObstructions[4]<2:
-                    moves+=(-2,1)
+                    moves.append((-2,1))
         if sideObstructions[1]<2:
+            if sideObstructions[2]==0:
+                moves.append((1,-2))
             if sideObstructions[3]==0:
-                moves+=(1,2)
-            if sideObstructions[4]==0:
-                moves+=(1,-2)
+                moves.append((1,2))
             if sideObstructions[1]==0:
+                if sideObstructions[2]<2:
+                    moves.append((2,-1))
                 if sideObstructions[3]<2:
-                    moves+=(2,1)
-                if sideObstructions[4]<2:
-                    moves+=(2,-1) #this part not finished due to inelegance (and using maximum 12 list lookups (you can do it in 8 if you're willing to repeat statements more, but I can't find an efficient way to do '(do c and (b if d)) if a else (b if e)' ))
+                    moves.append((2,1)) #I can't find an efficient way to do (i if e and b), (j if f and a), (k if f and c), (l if g and b), (m if g and d), (n if h and c), (o if h and a) and (p if e and d) where e implies a, f implies b, g implies c and h implies d (it can be implemented in eight lookups trivially if you're creating a circuit board but requires twelve with this method)
+        moves=[position+m[0]+m[1]*boardWidth for m in moves]
+            
     return moves
-
 def precomputeKnightMoves():
     return [findPieceMoves(5,i) for i in range(boardSquares)]
-#precomputedKnightMoves=precomputeKnightMoves()
+knightMovesPrecomputed=0
+precomputedKnightMoves=precomputeKnightMoves()
+knightMovesPrecomputed=1
 
+print("precomputed knight moves:",precomputedKnightMoves)
+#piecePermutations=[[[0,0]]*(boardSquares-2)]
 statesWithoutTurns=[] #list of all legal states (permutations of pieces) as lists of piece types and colours (0 if no piece)
 stateSquareAttacks=[] #list of whether each square is attacked and (if so) by whom
-for i in range(boardSquares):
-    iKingMoves=findPieceMoves(1,i) #the new Apple product
-    for j in range(boardSquares):
-        if not (i==j or j in iKingMoves):
-            statesWithoutTurns.append([[(1 if k==i or k==j else 0),(1 if k==j else 0)] for k in range(boardSquares)]) #each square in each state list (in the list of them) is a list of the piece and its colour
-            attackedSquares=[]
-            for k,s in enumerate(statesWithoutTurns[-1]): #cannot be list comprehension because it references the list as it constructs it (I am greatly saddened)
-                for l in findPieceMoves(s[0],k):
-                    if [l,s[1]] not in attackedSquares:
-                        attackedSquares.append([l,s[1]]) #will have to be made more complicated if pawns added
-                #print(attackedSquares)
-            #[0,0] is empty, [1,0] is white, [1,1] is black (I will eventually find how to make lists of bits to speed it up (like bitboards))
-            stateSquareAttacks.append([[int([k,l] in attackedSquares) for l in range(2)] for k in range(boardSquares)]) #[0,0] is empty, [1,0] white, [0,1] black, [1,1] both
+for r in combinations:
+    piecePermutations=generatePermutations(r,boardSquares-2-len(r))
+    #print("permutation lengths (should be "+str(boardSquares)+"):",[len(i) for i in piecePermutations])
+    for i in range(boardSquares):
+        iKingMoves=findPieceMoves(1,i) #the new Apple product
+        for j in range(boardSquares):
+            if not (i==j or j in iKingMoves):
+                kingState=[[int(k==i or k==j),int(k==j)] for k in range(boardSquares)] #each square in each state list (in the list of them) is a list of the piece and its colour
+                fixedKingPermutations=[[]]*len(piecePermutations) #not because the original ones are broken but because the kings are fixed in place
+                for p,q in enumerate(piecePermutations):
+                    m=-1
+                    fixedKingPermutations[p]=[l if l[0]==1 else q[m:=m+1] for l in kingState] #the walrus operator is quite cute I would say
+                print("fixed-king permutation lengths (should be "+str(boardSquares)+"):",[len(i) for i in fixedKingPermutations])
+                for p,q in enumerate(fixedKingPermutations):
+                    statesWithoutTurns.append(q) #each square in each state list (in the list of them) is a list of the piece and its colour
+                    attackedSquares=[]
+                    for k,s in enumerate(statesWithoutTurns[-1]): #cannot be list comprehension because it references the list as it constructs it (I am greatly saddened)
+                        for l in findPieceMoves(s[0],k):
+                            #print(s,m,statesWithoutTurns[-1])
+                            if ([l,s[1]] not in attackedSquares): #initially contained 'and (statesWithoutTurns[-1][m][0]==0 or statesWithoutTurns[-1][m][1]!=s[1])' but should not because then it doesn't recognise pieces defending each other
+                                attackedSquares.append([l,s[1]]) #will have to be made more complicated if pawns added
+                        #print(attackedSquares)
+                    #[0,0] is empty, [1,0] is white, [1,1] is black (may eventually be pairs of 64-bit binary numbers (like bitboards) when I need to optimise it)
+                    stateSquareAttacks.append([[int([k,l] in attackedSquares) for l in range(2)] for k in range(boardSquares)]) #[0,0] is empty, [1,0] white, [0,1] black, [1,1] both
 #print(len(statesWithoutTurns),"states without turns:",statesWithoutTurns)
 states=[[j,i] for j in range(2) for i in statesWithoutTurns] #each state is formatted [colour to move,[[[square piece],[square colour]],[[square piece],[square colour]], ...]]
 #print(len(states),"states:",states)
 stateSquareAttacks=[i for j in range(2) for i in stateSquareAttacks] #state formatted [[attacked by white?,attacked by black?] for square in range(boardSquares)] #get a load of the comments' opening square brackets' kerning
 #print(len(stateSquareAttacks),"state square attacks:",stateSquareAttacks)
-stateMoves=[[[j for j in findPieceMoves(s[1][i][0],i) if s[1][i][0]!=1 or stateSquareAttacks[a][j][1-s[0]]==0] if (s[1][i][1]==s[0]) else [] for i in range(boardSquares)] for a,s in enumerate(states)] #is list of lists of destination squares for each piece #the i list comprehension if statement would have (s[1][i][0]!=0 and ) but is now superseded by findPieceMoves
-#print(len(stateMoves),"state moves:",stateMoves)
-stateTransitions=[[states.index([(1-t[0]),[[0,0] if k==i else (t[1][i] if k==p else r) for k,r in enumerate(t[1])]]) for i,q in enumerate(stateMoves[s]) for j,p in enumerate(q)] for s,t in enumerate(states)]
+stateMoves=[[[j for j in findPieceMoves(t[1][i][0],i) if (t[1][j][0]==0 or t[1][i][1]!=t[1][j][1]) and (stateSquareAttacks[s][j][1-t[0]]==0 if t[1][i][0]==1 else t[1][j][0]!=1)] if t[1][i][1]==t[0] else [] for i in range(boardSquares)] for s,t in enumerate(states)] #is list of lists of destination squares for each piece #the i list comprehension if statement would have (s[1][i][0]!=0 and ) but is now superseded by findPieceMoves
+stateTransitions=[[states.index([1-t[0],[[0,0] if k==i else (t[1][i] if k==p else r) for k,r in enumerate(t[1])]]) for i,q in enumerate(stateMoves[s]) for p in q] for s,t in enumerate(states)]
 print("transitions between",len(states),"states:",stateTransitions)
 
 def underline(input): #from https://stackoverflow.com/a/71034895
@@ -97,12 +134,9 @@ def printBoard(board):
         if (i%boardWidth+math.floor(i/boardWidth))%2==1: #light square
             letter=underline(letter)
         stateToPrint+=letter
-        #print(stateToPrint)
         if (i+1)%boardWidth==0:
-            output+=stateToPrint
-            output+='''
+            output+=stateToPrint+'''
 '''
-            #print(output)
             stateToPrint=""
     return output
 
@@ -111,7 +145,7 @@ black=(0,0,0)
 squareColours=((236,217,185),(174,137,104)) #I am using lichess's square colours (I am wanted throughout NATO)
 dims=3
 size=[1050]*dims #my computer resolution (to let NATO catch me more easily)
-screen = pygame.display.set_mode((size[0], size[1]))
+screen = pygame.display.set_mode(size[0:2])
 cameraPosition=[i/2 for i in size]
 cameraAngle=[[1]+[0]*3]*2
 clickDone=0
@@ -127,17 +161,17 @@ def physics():
     for i in squares:
         if drag>0:
             absVel=max(1,math.sqrt(sum([i[0][di][1]**2 for di in range(dims)]))) #each dimension's deceleration from drag is its magnitude as a component of the unit vector of velocity times absolute velocity squared, is actual component times absolute velocity.
-            for di in range(dims):
-                i[0][di][1]*=1-absVel*drag #air resistance
-        for di in range(dims):
-            i[0][di][0]+=i[0][di][1]
+            for di in i[0]:
+                di[1]*=1-absVel*drag #air resistance
+        for di in i[0]:
+            di[0]+=di[1]
     for i,k in enumerate(squares[:-1]):
-        for j,l in enumerate(squares[i:]):
+        for j,l in enumerate(squares[i+1:]):
             differences=[l[0][di][0]-k[0][di][0] for di in range(dims)]
-            gravity=gravitationalConstant/max(1,math.sqrt(sum([di**2 for di in differences])**3))
+            gravity=gravitationalConstant/max(1,sum([di**2 for di in differences])**1.5) #inverse-square law is 1/distance**2, for x axis is cos(angle of distance from axis)/(absolute distance)**2, the cos is x/(absolute), so is x/(abs)**3, however the sum outputs distance**2 so is exponentiated by 1.5 instead of 3
             for di in range(dims):
-                k[0][di][1]+=differences[di]*(hookeStrength*(i+j in stateTransitions[i])+gravity*l[2])
-                l[0][di][1]-=differences[di]*(hookeStrength*(i in stateTransitions[i+j])+gravity*k[2])
+                k[0][di][1]+=differences[di]*(hookeStrength*(i+1+j in stateTransitions[i])+gravity*l[2])
+                l[0][di][1]-=differences[di]*(hookeStrength*(i in stateTransitions[i+1+j])+gravity*k[2])
 def drawShape(size,pos,colour,shape):
     if shape==0:
         surf = pygame.Surface(size)
@@ -153,7 +187,7 @@ def quaternionMultiply(a,b):
     return [a[0]*b[0]-a[1]*b[1]-a[2]*b[2]-a[3]*b[3],
             a[0]*b[1]+a[1]*b[0]+a[2]*b[3]-a[3]*b[2],
             a[0]*b[2]-a[1]*b[3]+a[2]*b[0]+a[3]*b[1],
-            a[0]*b[3]+a[1]*b[2]-a[2]*b[1]+a[3]*b[0]]
+            a[0]*b[3]+a[1]*b[2]-a[2]*b[1]+a[3]*b[0]] #this function not to be taken to before 1843
 
 def quaternionConjugate(q):
     return [q[0]]+[-i for i in q[1:4]]
@@ -179,6 +213,7 @@ def findSquareScreenPositions():
 
 gain=1
 angularVelocityConversionFactor=math.tau/FPS
+mouse=pygame.mouse
 run=True
 while run:
     for event in pygame.event.get():
@@ -186,11 +221,11 @@ while run:
         clickDone=event.type == pygame.MOUSEBUTTONUP
     cameraPosition=[sum([i[0][di][0] for i in squares])/len(squares)-size[di]/2 for di in range(dims)]
     if dims==3:
-        mouse=pygame.mouse
         keys=pygame.key.get_pressed()
         if mouse.get_pressed()[0]==1:
             mouseChange=mouse.get_rel()
         else:
+            mouse.get_rel() #otherwise it jumps
             mouseChange=(0,)*2
         arrowAccs=[0]+[keys[pygame.K_UP]-keys[pygame.K_DOWN],keys[pygame.K_RIGHT]-keys[pygame.K_LEFT],keys[pygame.K_e]-keys[pygame.K_q]] 
         magnitude=math.sqrt(sum([abs(i) for i in arrowAccs]))

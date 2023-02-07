efdbfdd7580ae2eb98062d876f417ad407de68b1
DroneBetter <58664547+DroneBetter@users.noreply.github.com>
2023-02-07 13:27:26 +0000

    Add necklace mode (for Wolfram cylinders)
    I made a simple and suboptimal program for enumerating them, with which I found the applicable OEIS sequences (A000029, A000031, A102659) and read about Duval's algorithm (the 1988 one for iterating over them, the idea being that for k-ary necklaces, you count in k-ary by XORing the last bit with 1 each iteration as usual, but instead of replacing the trailing 1s (or k-1s) with 0s, you replace them with a tiling of the entire integer (up to the necklace bound) preceding their beginning, and only need to check that this occurred without leaving a remainder), I found David Eppstein's implementation at https://gist.github.com/dvberkel/1950267 and implemented it for binary necklaces myself with bitwise operators (it is one of the most elegant ones I have ever written (implementing without loops (except for a single one for the tiling, which takes logarithmically many iterations), using Chai Wah Wu's expression for A007814 (that was only added in 2022-07-01)), though in my testing it's surprisingly only twice as fast as Eppstein's one (that operates on list representations), I suppose duplicating elements is very efficient also)
    It seems a very difficult task to implement indexing methods (there is https://arxiv.org/pdf/1504.00572.pdf about it, which I don't understand (but will one day, I hope)), so for now it enables hybrid mode if scroll is enabled so it doesn't have to (which I think would probably save time anyway)
    I don't like the canonical/symmetry functions for it, but can find no faster (non-equivalent) way to do them (and it seems neither could Eppstein)
    Anyway, as-is, it seems that the reduction by cyclic shift in the initialisation yields massive speedup
    Here is the slightly unravelled version of __iter__ (in the format of LyndonWordsWithLength (and equivalent if not self.subperiods)) if you would like to use it for your own purposes :-)
    def __iter__(self):
        if self.n:
            l=w=1
            while True:
                w=reduce(lambda w,i: w|w>>(l<<i),range(l and self.n//l),(w^1)<<self.n-l)
                if (not self.n%l if self.subperiods else self.n==l):
                    yield(w)
                t=(w&~w-1).bit_length()
                if t<self.n:
                    w>>=t
                    l=self.n-t
                else:
                    break
        else:
            yield(0)

--- twofold_reducer.py
+++ twofold_reducer.py
@@ -1,5 +1,7 @@
 from functools import reduce
 from itertools import accumulate,pairwise,starmap
+from math import isqrt
+from copy import deepcopy
 
 dbg=(lambda x,*s: (x,print(*s,x))[0]) #debug
 revange=(lambda a,b=None,c=1: range(b-c,a-c,-c) if b else range(a-c,-c,-c)) #(lambda *a: reversed(range(*a)))
@@ -33,43 +35,133 @@ def redwhile(f,c,l,i=None): #reduce but with a condition also
         i=f(i,j)
         if c(i): return(b(i))
     return(o(i))'''
-def shortduce(f,l,i=None,o=None,b=None): #redwhile but with different function depending on shortcut (second element is used only as whether to proceed)
+def shortduce(f,l=None,i=None,o=None,b=None): #redwhile but with different function depending on shortcut (second element is used only as whether to proceed)
     if i==None: i=next(l)
     i=(i,True)
-    for j in l:
-        if i[1]: i=f(i[0],j)
-        else: return(i[0] if b==None else b(i[0]))
+    if l==None:
+        while True:
+            if i[1]: i=f(i[0])
+            else: return(i[0] if b==None else b(i[0]))
+    else:
+        for j in l:
+            if i[1]: i=f(i[0],j)
+            else: return(i[0] if b==None else b(i[0]))
     return((lambda f,i: i if f==None else f(i))(o if i[1] else b,i[0]))
+
+#these parts from https://github.com/DroneBetter/DroneLambda/blob/main/DroneLambda.py but without right-associativity
+strucget=(lambda struc,inds: reduce(lambda a,b: a[b],inds[:-1],struc))
+def strucset(struc,inds,val): #function call cannot be assigned to directly for whatever reason
+    if len(inds)>1:
+        strucget(struc,inds[:-1])[inds[-2]]=val
+    else:
+        struc=val
+    return(struc)
+def structrans(struc,f=None,lf=None,rev=False,fints=False):
+    inds=[len(struc)-1 if rev else 0]
+    b=False
+    while len(inds) and type(struc)==list and 0<=inds[0]<len(struc):
+        if f:
+            (struc,inds)=f(struc,inds)
+        if type(strucget(struc,inds))==list:
+            if lf:
+                (struc,inds)=lf(struc,inds)
+            inds.append(len(strucget(struc,inds+[0]))-1 if rev and type(strucget(struc,inds+[0]))==list else 0)
+        else:
+            del inds[-1]
+            if len(inds):
+                inds[-1]+=(-1)**rev
+        while len(inds) and (type(strucget(struc,inds))==int or (inds[-1]<0 if rev else len(strucget(struc,inds))<=inds[-1])):
+            if f and fints and type(strucget(struc,inds))==int:
+                (struc,inds)=f(struc,inds)
+            del inds[-1]
+            if not inds:
+                b=True
+                break
+            inds[-1]+=(-1)**rev
+        if len(inds) and b: break
+    if f and (type(struc)==list or fints):
+        struc=f(struc,[0])[0]
+    if lf and type(struc)==list:
+        struc=lf(struc,[0])[0]
+    return(struc)
+def enmax(struc,f,i=None,fints=False,iints=False):
+    muc=deepcopy(struc)
+    if i:
+        muc=structrans(muc,i,fints=iints)
+    #print(struc)
+    global diff
+    diff=True
+    while diff:
+        diff=False
+        muc=structrans(muc,f,fints=fints)
+    return(muc)
 
 andMasks=(lambda n: (lambda bitWidth: tuple(reduce(lambda n,j: n|n<<(1<<j),range(i+1,bitWidth),(1<<(1<<i))-1) for i in range(bitWidth)))((n-1).bit_length()))
 
 class twofold:
-    def __init__(self,n,symmetry=True):
+    factorise=(lambda self,n: (lambda f: f+tap(lambda f: n//f,reversed(f[:-1] if isqrt(n)**2==n else f)))(tuple(filter(lambda a: n%a==0,range(1,isqrt(n)+1))))) #0 does NOT divide positive integers (take your medicine)
+    def stractorise(self,struc,inds): #structure factorise
+        global diff
+        if (lambda g: type(g)==int and g!=1)(strucget(struc,inds)) and (lambda g: len(g)==inds[-2]+1 or type(g[inds[-2]+1])==int)(strucget(struc,inds[:-1])):
+            diff=True
+            struc=strucset(struc,inds,(lambda g: [g,list(self.factorise(g))[1:-1]])(strucget(struc,inds)))
+        return(struc,inds)
+    primate=(lambda self,n: () if n==1 else (lambda p: p if p else ((n,1),))(tuple(filter(lambda p: p[1],map(lambda p: (p,shortduce(lambda i: (i[0],False) if i[1]%p else ((i[0]+1,i[1]//p),True),i=(0,n))),reduce(lambda t,i: t+(i,)*all(map(lambda p: i%p,t)),range(2,n),())))))) #perhaps I will make a range(2,isqrt(n)+1) version one day
+    phi=(lambda self,n: 1 if n==1 else reduce(int.__mul__,starmap(lambda p,e: p**(e-1)*(p-1),self.primate(n)),1))
+    mu=(lambda self,n: (lambda p: 0 if any(map(lambda f: f[1]>1,p)) else (-1)**len(p))(self.primate(n)))
+    lengther=(lambda self,n,o: sum(map(lambda f: o(f)<<n//f,self.factorise(n)))//n if n else 1)
+    def __init__(self,n,symmetry=True,scroll=False,subperiods=True):
         self.n=n
         self.sym=symmetry
-        self.length=((1<<(n+1>>1)|1<<n)>>1 if symmetry else 1<<n)
+        self.bits=(1<<n)-1
+        self.scroll=scroll
+        self.subperiods=subperiods
+        if scroll:
+            prime=(lambda n: len(self.primate(n))==2) #Donald Knuth would be rolling in his grave
+            self.factors=self.factorise(n)
+            #print('n',n,self.factors)#,tap(self.phi,self.factors))
+            depthen=(lambda f: lap(lambda f: list(self.factorise(f)),f[:-1])+[f[-1]])
+            self.factree=enmax([n],self.stractorise,fints=True)[0]
+            #print(self.factree)
+            self.sublengths=tap(lambda f: self.lengther(f,self.mu),self.factorise(n)) #number of period-f tapes
+            #print('l',self.length,self.sublengths)
+            self.actions=tuple(range(n))
+            self.compositions=tap(lambda a: tap(lambda i: a*i%n,range(n)),self.actions)
+            self.roots=tap(lambda a: tap(lambda i: tap(lambda n: n[0],filter(lambda n: n[1]==a,starmap(lambda e,r: (e,r[i]),enumerate(self.compositions)))),range(n)),self.actions)
+            #print('a',self.actions)
+            #print('c',self.compositions)
+            #print('r\n'+'\n'.join(starmap(lambda i,r: str(i)+' '+str(r),enumerate(self.roots))))
+            invariants=(lambda p: (lambda m: tap(lambda i: i*m,range(1<<p)))(ORsum(map(lambda i: 1<<p*i,range(n//p)))))
+            #self.struc=tap(invariants,self.factors)
+            #self.struc=tap(lambda f: (lambda m: tuple(filter(lambda i: all(map(lambda m: i^(i>>m|i<<n-m&self.bits),m[:-1])),invariants(f))))(self.factorise(f)),self.factors)
+            #print('s',tap(lambda s: tap(bin,s),self.struc))
+        self.length=((lambda l: (l>>1)+(1<<(n>>1) if n&1 else 0b11<<(n>>1)-2) if symmetry else l)(self.lengther(n,self.phi)) if scroll else ((1<<(n+1>>1)|1<<n)>>1 if symmetry else 1<<n))
         #from reversalParameters in https://gist.github.com/DroneBetter/4f5d775e7c37f062ce750d4a8fefc84a (not sure how else to define instance-specific open-form function (which it must be due to in-places))
-        masks=[[[m,1<<i,False],[m,0,True]] for i,m in enumerate(andMasks(n))]
-        exceeding=(1<<(n-1).bit_length())-n
-        if exceeding:
-            endShifts=[-exceeding//2,exceeding//2]
-            for i,m in enumerate(masks):
-                #print(m[0][1])
-                shifter=(lambda i,m: min(m[0][1],m[0][1]-2*(1<<i),key=abs))
-                if -endShifts[0]<=shifter(i,m): m[0][1]+=endShifts[0]; m[1][0]>>=-endShifts[0]; break
-                else:
-                    m[0][0]>>=m[0][1]-endShifts[0]; m[0][1]-=endShifts[0]; m[0][2]=True; m[1][0]>>=-endShifts[0]
-                    common=(shifter(i,m) if m[0][1]^(m[0][1]-2*(1<<i))>0 else 0)
-                    if common:
-                        endShifts[0]=-common
-                        if endShifts[0]<m[0][1]: m[0][1]=0
-                    else: break
-            masks[-1][0][0]>>=endShifts[1]; masks[-1][0][1]+=endShifts[1]
-        for i,m in enumerate(masks): m[1][1]=m[0][1]-2*(1<<i)
-        conditionalSwap=(lambda x,b,t,f,br: '('*br+x+(t+')'*br+f if b else f+')'*br+t))
-        indent=(lambda i,t: " "*((i-len(t))*(i>len(t)))+t)
-        marge=[0,0]#[max(len(str((abs if i else hex)(n[i]))) for m in masks for n in m) for i in range(2)]
-        self.reverse=eval(("lambda x: "+"".join("(lambda x: "+'|'.join(conditionalSwap('x',(n[2] and n[1]),"&"+indent(marge[0],str(hex(n[0]))),((">>" if n[1]>0 else "<<")+indent(marge[1],str(abs(n[1]))) if n[1] else ' '*(2+marge[1])),(n[1] and n[2])) for n in m)+')(' for m in masks[::-1])+'x'+')'*len(masks)))
+        def reversalParameters(n):
+            masks=[[[m,1<<i,False],[m,0,True]] for i,m in enumerate(andMasks(n))]
+            exceeding=(1<<(n-1).bit_length())-n
+            if exceeding:
+                endShifts=[-exceeding//2,exceeding//2]
+                for i,m in enumerate(masks):
+                    #print(m[0][1])
+                    shifter=(lambda i,m: min(m[0][1],m[0][1]-2*(1<<i),key=abs))
+                    if -endShifts[0]<=shifter(i,m): m[0][1]+=endShifts[0]; m[1][0]>>=-endShifts[0]; break
+                    else:
+                        m[0][0]>>=m[0][1]-endShifts[0]; m[0][1]-=endShifts[0]; m[0][2]=True; m[1][0]>>=-endShifts[0]
+                        common=(shifter(i,m) if m[0][1]^(m[0][1]-2*(1<<i))>0 else 0)
+                        if common:
+                            endShifts[0]=-common
+                            if endShifts[0]<m[0][1]: m[0][1]=0
+                        else: break
+                masks[-1][0][0]>>=endShifts[1]; masks[-1][0][1]+=endShifts[1]
+            for i,m in enumerate(masks): m[1][1]=m[0][1]-2*(1<<i)
+            conditionalSwap=(lambda x,b,t,f,br: '('*br+x+(t+')'*br+f if b else f+')'*br+t))
+            indent=(lambda i,t: " "*((i-len(t))*(i>len(t)))+t)
+            marge=[0,0]#[max(len(str((abs if i else hex)(n[i]))) for m in masks for n in m) for i in range(2)] except it is one line
+            return(eval("lambda x: "+"".join("(lambda x: "+'|'.join(conditionalSwap('x',(n[2] and n[1]),"&"+indent(marge[0],str(hex(n[0]))),((">>" if n[1]>0 else "<<")+indent(marge[1],str(abs(n[1]))) if n[1] else ' '*(2+marge[1])),(n[1] and n[2])) for n in m)+')(' for m in masks[::-1])+'x'+')'*len(masks)))
+        self.reverse=reversalParameters(n)
+        if scroll:
+            self.subreverses=tap(reversalParameters,range(n+1))
 
     __len__=(lambda self: self.length)
     strate=(lambda self,s: ''.join(map(lambda i: 'o' if s>>i&1 else ' ',range(self.n))))
@@ -77,11 +169,26 @@ class twofold:
     layer=(lambda self,s,i: s>>i&1|s>>self.n-i-2&2)
     layinc=(lambda self,s,b,i: (lambda l: ((s&~(1<<i|1<<self.n+~i),b),True) if l==3 else ((((s|1<<i,i if i==b+1 else b) if l==2 else (s&~(1<<i)|1<<self.n+~i,b) if l else (s|1<<i,b)) if i>b else (s|1<<self.n+~i,b)) if l else (s|1<<i,min(i,b)),False))(self.layer(s,i))) #2 cannot be reached in layers under symmetry
     branch=(lambda self,s: (lambda i: i+(i==self.n//2 and self.n&1))(iindex(map(lambda i: s>>i&1^s>>self.n+~i&1,range(self.n//2)),1)))
-    symmetry=(lambda self,s: funcxp(self.reverse,(lambda b: b<=self.n//2 and self.layer(s,b)==2)(self.branch(s)))(s) if self.sym else s)
-    nexter=(lambda self,s,b: (s|1<<self.n//2,b) if self.n&1 and not s&1<<self.n//2 else shortduce(lambda s,i: self.layinc(*s,i),revange(self.n//2),(s&~(1<<self.n//2) if self.n&1 else s,b))) #internal (for conserving the branch output in between)
-    __next__=(lambda self,s: self.nexter(s,self.branch(s))[0] if self.sym else s+1&(1<<self.n)-1) #will loop upon itself endlessly (I hate StopIteration)
+    symmetry=(lambda self,s: min(map(lambda s: min(redumulate(lambda s,i: s>>1|(s&1)<<self.n-1,range(self.n),s)),((s,self.reverse(s)) if self.sym else (s,)))) if self.scroll else funcxp(self.reverse,(lambda b: b<=self.n//2 and self.layer(s,b)==2)(self.branch(s)))(s) if self.sym else s)
+    canonical=(lambda self,s: s==self.symmetry(s)) #whether it is canonical (in scrolling version) #(lambda self,s,l: (lambda b: b>l//2 or (s>>b&1 or not s>>l+~b&1))((lambda i: i+(i==l//2 and l&1))(iindex(map(lambda i: s>>i&1^s>>l+~i&1,range(l//2)),1)))) breaks down for n>=11, unfortunately
+    def nexter(self,w,l): #internal (for conserving the l or b output in between)
+        if self.scroll:
+            while True: #runs for asymptotically constantly many iterations before termination
+                t=(w&~w-1).bit_length()
+                if t<self.n:
+                    w>>=t
+                    l=self.n-t
+                    w=reduce(lambda w,i: w|w>>(l<<i),range(l and self.n//l),(w^1)<<self.n-l)
+                    if (not self.n%l if self.subperiods else self.n==l) and (not self.sym or self.canonical(w)):#self.canonical(w,w.bit_length())):
+                        return(w,l)
+                else:
+                    raise(StopIteration)
+        else:
+            return((w|1<<self.n//2,l) if self.n&1 and not w&1<<self.n//2 else shortduce(lambda w,i: self.layinc(*w,i),revange(self.n//2),(w&~(1<<self.n//2) if self.n&1 else w,l)))
+    period=(lambda self,w: next(filter(lambda m: not w^(w>>m|w<<self.n-m&self.bits),self.factors)))
+    __next__=(lambda self,s: self.nexter(s,(self.period if self.scroll else self.branch)(s))[0] if self.sym or self.scroll else s+1&(1<<self.n)-1) #will loop upon itself endlessly (I hate StopIteration)
     #__iter__=(lambda self: redumulate(lambda s,i: t.__next__(s),range(len(self)-1),0)) #trivial implementation
-    __iter__=(lambda self,l=None,u=None: map(lambda i: i[0],redumulate(lambda s,i: self.nexter(*s),range((len(self) if u==None else u if l==None else u-l)-1),(0,self.n) if l==None else (lambda s: (s,self.branch(s)))(self[l]))) if self.sym else iter(range(len(self)) if l==None else range(l) if u==None else range(l,u))) #does not compute branch
+    __iter__=(lambda self,l=None,u=None: map(lambda i: i[0],redumulate(lambda s,i: self.nexter(*s),range((len(self) if u==None else u if l==None else u-l)-1),(0,1 if self.scroll else self.n) if self.scroll or l==None else (lambda s: (s,self.branch(s)))(self[l]))) if self.sym or self.scroll else iter(range(len(self)) if l==None else range(l) if u==None else range(l,u))) #does not compute branch
     #polya=(lambda self,l,i,b: (3<<(self.n&~1)-2*(i+1) if i>b else (1<<(self.n&~1)-2*(i+1))+(((1<<((self.n>>1)-1-i))+(1<<(self.n&~1)-2*(i+1)))>>1)) if l==3 else (2<<(self.n&~1)-2*(i+1) if i>b else (1<<(self.n&~1)-2*(i+1))+((1<<((self.n>>1)-1-i))+(1<<(self.n&~1)-2*(i+1))>>1)) if l==2 else (1<<(self.n&~1)-2*(i+1) if i>b else ((1<<((self.n>>1)-1-i))+(1<<(self.n&~1)-2*(i+1))>>1)) if l else 0)
     polya=(lambda self,l,i,b: l<<(self.n&~1)+(~i<<1) if b else (l<<(self.n&~1)+(~i<<1))+(1<<(self.n>>1)+~i)>>1 if l else 0)
     index=(lambda self,s,sym=True: (lambda s: (lambda b: sum(map(lambda i: self.polya(self.layer(s,i),i,i>b),range(self.n>>1)))<<(self.n&1)|self.n&s>>(self.n>>1)&1)(self.branch(s)))(funcxp(self.symmetry,sym)(s)) if self.sym else s&(1<<self.n)-1)
@@ -96,6 +203,8 @@ if __name__=='__main__':
     print('|'.join(map(lambda i: t.strate(t.symmetry(t.reverse(t[i]))),range(len(t)))))
     #print('|'.join(map(t.strate,t[:4])))
     print(tuple(map(t.index,t))==tuple(range(len(t))))'''
+    #print((lambda n: '\n'.join(map(n.strate,n)))(twofold(11,False,True)))
+    #print((lambda n: '\n'.join(map(n.strate,n)))(twofold(11,True,True)));exit()
     from time import time
     ankos=('0',        '~(p|q|r)',    '~p&~q&r',    '~(p|q)',      '~(p|r)&q',   '~(p|r)',      '~p&(q^r)',   '~(p|q&r)',      '~p&q&r',       '~(p|q^r)',      '~p&r',        'p^(p|~q|r)',    'p&q^q',       'p^(p|q|~r)',    'p^(p|q|r)',   '~p',
            'p&~q&~r',  '~(q|r)',      '(p^q^r)&~q', '~(p&r|q)',    '(p^q)&~r',   '~(p&q|r)',    'p^p&q&r^q^r','p^(p^~q|q^r)',  '(p^q)&(p^r)',  'p&q&r^q^~r',    'p^(p&q|r)',   'p^(p^~q|r)',    'p^(p&r|q)',   'p^(p^~r|q)',    'p^(q|r)',     '~(p&(q|r))',
@@ -134,13 +243,14 @@ if __name__=='__main__':
     #print(tap(lambda x: map(hex,x),filter(lambda x: x[0]!=x[1],enumerate(starmap(lambda i,w: ORsum(map(lambda i: (w(i>>2,i>>1&1,i&1)&1)<<i,range(8))),enumerate(wolfram))))))
     rule=150
     boundaries=0 #left and right fixed edges
+    scroll=True
     cut=True #takes linear space this way, to avoid running over high-period oscillators too many times
     symmetry=(lambda r: ~(r|r>>2&1)&1)(rule>>1^rule>>4) #~((rule>>1^rule>>4)|(rule>>3^rule>>6))&1
-    hybrid=True #do not use symmetry-reduced indexing methods (only twofold.__iter__)
+    hybrid=True or scroll #do not use symmetry-reduced indexing methods (only twofold.__iter__)
     reduction=True #reduce state by symmetry but do not use optimal-space searcheds list
     fast=2#+(rule==150)
     fun=wolfram[rule]
-    wr=(lambda s: fun(s>>1|boundaries>>1<<w-1,s,s<<1|boundaries&1)&mask)
+    wr=(lambda s: (fun(s>>1|(s&1)<<w-1,s,s<<1|s>>w-1) if scroll else fun(s>>1|boundaries>>1<<w-1,s,s<<1|boundaries&1))&mask)
     canon=(lambda p: min(map(f.symmetry,p)))
     periods=[]
     allPeriods=set()
@@ -152,7 +262,9 @@ if __name__=='__main__':
         mask=(1<<w)-1
         #print('width',w)
         n=1<<16
-        f=twofold(w,isotropic)
+        print(w,isotropic,scroll)
+        
+        f=twofold(w,isotropic,scroll)
         if cut: searcheds=[False for _ in range(1<<w if hybrid else len(f))]
         finals=set()
         t=time()
